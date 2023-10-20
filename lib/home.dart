166a30401ea1e9177f8af41f4bee0d4abace1554
sidneyojr <sidney.o@aluno.ifsp.edu.br>
2023-10-20 00:05:47 -0300

    Build web

---
+++ lib/home.dart
@@ -0,0 +1,49 @@
+//import 'package:autoleitura/login.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('AUTO LEITURA'),
+        backgroundColor: Colors.blue, // Manter a app bar no azul
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Seja Bem-Vindo ao sistema de Auto Leitura"),
+            SizedBox(height: 25),
+            Material(
+              color: Colors.blueAccent, // Azul mais claro
+              borderRadius: BorderRadius.circular(20.0),
+              child: InkWell(
+                borderRadius: BorderRadius.circular(20.0),
+                onTap: () {
+                  //Navigator.push(
+                  //  context,
+                  //MaterialPageRoute(builder: (context) => Login()),
+                  //);
+                },
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                  ),
+                  child: Text(
+                    "Clique para inserir seu código",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
