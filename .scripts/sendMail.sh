81876068b3f6f9972b77d3c9b0ccb5a2028cdc82
sidneyojr <sidney.o@aluno.ifsp.edu.br>
2023-10-23 23:49:59 -0300

    Pipeline4

---
+++ .scripts/sendMail.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+sendEmail -f sidney.oliveira.projeto@zohomail.com \
+  -t  sidneyoliveirajunior@gmail.com \
+  -s smtp.zoho.com:587 \
+  -u "Teste via Pipeline" \
+  -m "Email enviado com o sendEmail! com Gihub actions" \
+  -xu sidney.oliveira.projeto@zohomail.com \
+  -xp '112PUwwnt8eM' \
+  -o tls=yes
+
