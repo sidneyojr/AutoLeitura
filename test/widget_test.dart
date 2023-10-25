2b31f0b72b72f573eafafeaf7b35877d950fac33
sidneyojr <sidney.o@aluno.ifsp.edu.br>
2023-10-25 15:00:19 -0300

    Pipeline Add e Adjusted 11 (Run Testes) Job: Teste

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:autoleitura/main.dart';
 
@@ -15,8 +15,9 @@ void main() {
     await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(
+        find.text('Seja Bem-Vindo ao sistema de Auto Leitura'), findsOneWidget);
+    //expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
     //await tester.tap(find.byIcon(Icons.add));
