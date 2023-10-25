f1292c37b7e6785af3917e025a9af11c85fc49c1
sidneyojr <sidney.o@aluno.ifsp.edu.br>
2023-10-25 16:21:00 -0300

    Pipeline Add e Adjusted 18 (Run Testes) Job: Teste

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,8 +5,8 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-//import 'package:flutter/material.dart';
 import 'package:autoleitura/login.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:autoleitura/main.dart';
 //import 'package:autoleitura/login.dart';
@@ -21,16 +21,19 @@ void main() {
         find.text('Seja Bem-Vindo ao sistema de Auto Leitura'), findsOneWidget);
 
     // Tap the "Clique aqui para inserir seu código" button.
-    await tester.tap(find.text('Clique aqui para inserir seu código'));
+    // Find the button by its key or text.
+    final buttonFinder = find.widgetWithText(
+        ElevatedButton, 'Clique aqui para inserir seu código');
+    expect(buttonFinder, findsOneWidget);
+
+    // Tap the button to trigger the action.
+    await tester.tap(buttonFinder);
 
     // Wait for the widget tree to settle.
     await tester.pumpAndSettle();
 
     // Verify that we are on the login screen.
     expect(find.byType(Login), findsOneWidget);
-
-    //expect(find.text('1'), findsNothing);
-
     // Tap the '+' icon and trigger a frame.
     //await tester.tap(find.byIcon(Icons.add));
     //await tester.pump();
