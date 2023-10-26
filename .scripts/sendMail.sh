6117654d8e9b758bb512bcd4c93594294e638ab0
sidneyojr <sidney.o@aluno.ifsp.edu.br>
2023-10-26 00:23:04 -0300

    Build13

--- .scripts/sendMail.sh
+++ .scripts/sendMail.sh
@@ -4,6 +4,7 @@ sendEmail -f sidney.oliveira.projeto@zohomail.com \
   -s smtp.zoho.com:587 \
   -u "Teste via Pipeline" \
   -m "Email enviado com o sendEmail! com Gihub actions" \
+  -a $ARTEFATO \
   -xu sidney.oliveira.projeto@zohomail.com \
   -xp '112PUwwnt8eM' \
   -o tls=yes
