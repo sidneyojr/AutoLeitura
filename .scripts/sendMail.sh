9df02542eeae5570aa82473b43ceaee6f0deb836
sidneyojr <sidney.o@aluno.ifsp.edu.br>
2023-10-26 15:36:25 -0300

    Build29

--- .scripts/sendMail.sh
+++ .scripts/sendMail.sh
@@ -3,8 +3,8 @@
 sendEmail -f sidney.oliveira.projeto@zohomail.com \
   -t  sidneyoliveirajunior@gmail.com \
   -s smtp.zoho.com:587 \
-  -u "Teste via terminal" \
-  -m "<h1>Uma nova Versão de homologação está disponivel para ser instlado</h1>" \
+  -u "Versão Homologação Auto Leitura" \
+  -m "<h1>Uma nova Versão de homologação está disponivel para ser instlado!!</h1>" \
   -a  ./.deploy_homol/README.md \
   -xu sidney.oliveira.projeto@zohomail.com \
   -xp '112PUwwnt8eM' \
