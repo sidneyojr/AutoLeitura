25cd8f47e84be208fefc3440191e330e6ad10fbc
sidneyojr <sidney.o@aluno.ifsp.edu.br>
2023-10-26 14:11:51 -0300

    Build23

--- .scripts/sendMail.sh
+++ .scripts/sendMail.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
+
 sendEmail -f sidney.oliveira.projeto@zohomail.com \
   -t  sidneyoliveirajunior@gmail.com \
   -s smtp.zoho.com:587 \
-  -u "Teste via Pipeline" \
-  -m "Email enviado com o sendEmail! com Gihub actions" \
-  -a .deploy_homol/app-release.apk
+  -u "Teste via terminal" \
+  -m "Email enviado com o sendEmail!" \
   -xu sidney.oliveira.projeto@zohomail.com \
   -xp '112PUwwnt8eM' \
   -o tls=yes
-
+  
