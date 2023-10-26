f6a7764434f2796a768bd7a7eba18d20a7dc7eab
sidneyojr <sidney.o@aluno.ifsp.edu.br>
2023-10-26 15:17:07 -0300

    Build28

--- .scripts/sendMail.sh
+++ .scripts/sendMail.sh
@@ -4,7 +4,7 @@ sendEmail -f sidney.oliveira.projeto@zohomail.com \
   -t  sidneyoliveirajunior@gmail.com \
   -s smtp.zoho.com:587 \
   -u "Teste via terminal" \
-  -m "Email enviado com o sendEmail!" \
+  -m "<h1>Uma nova Versão de homologação está disponivel para ser instlado</h1>" \
   -a  ./.deploy_homol/README.md \
   -xu sidney.oliveira.projeto@zohomail.com \
   -xp '112PUwwnt8eM' \
