0272a341a3a264bd3201909f2fe77d96973125eb
Sidney de Oliveira Junior <57119046+sidneyojr@users.noreply.github.com>
2023-10-30 17:20:13 -0300

    Update sendMail.sh

--- .scripts/sendMail.sh
+++ .scripts/sendMail.sh
@@ -4,8 +4,8 @@ sendEmail -f sidney.oliveira.projeto@zohomail.com \
   -t  sidneyoliveirajunior@gmail.com \
   -s smtp.zoho.com:587 \
   -u "Versão Homologação Auto Leitura" \
-  -m "<h1>Uma nova Versão de homologação está disponivel para ser instlado!!</h1>" \
-  -a  ./.deploy_homol/README.md \
+  -m "<h1>Uma nova Versão de homologação está disponivel para ser instalado!!</h1>" \
+  -a  ./.deploy_homol/app-release.apk \
   -xu sidney.oliveira.projeto@zohomail.com \
   -xp '112PUwwnt8eM' \
   -o tls=yes
