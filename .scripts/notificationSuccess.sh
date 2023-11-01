824f3b2da02cc224229f56e3094d5e4e7085c5e5
sidneyojr <sidney.o@aluno.ifsp.edu.br>
2023-10-31 23:45:50 -0300

    Notificação por Telegram teste de Cobertura2

--- .scripts/notificationSuccess.sh
+++ .scripts/notificationSuccess.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -X POST "https://api.telegram.org/bot5352882450:AAElAs4XYp0r5gYHoWrovu7-WLYcKjSb2Qk/sendMessage" -d "chat_id=-783035082&text=Pipeline Executada com SUCESSO"
+curl -s -X POST https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage -d chat_id=$TELEGRAM_CHAT_ID -d text="Pipeline Executada com SUCESSO"
