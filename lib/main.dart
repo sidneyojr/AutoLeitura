476ac26091e6bef3fbd26f6dc024b9b9ba9cc6be
sidneyojr <sidney.o@aluno.ifsp.edu.br>
2023-12-04 18:45:58 -0300

    Preparando para Melhoria nas exibição das telas

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class AutoLeitura extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: AutoLeituraScreen(),
     );
   }
